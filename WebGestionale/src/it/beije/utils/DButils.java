eecc9e91855c6a1b790fc7ba016495c18772e693
Padawan03 <Padawan03@LAPTOP-9GTSBHJH.lan>
2019-03-12 17:59:06 +0100

    Collegamento DB Inserimento dati Cerca dipendente Visualizza dipendente
    Modifica il modificadipendente() in Dbutils

--- WebGestionale/src/it/beije/utils/DButils.java
+++ WebGestionale/src/it/beije/utils/DButils.java
@@ -8,8 +8,8 @@ import java.sql.Statement;
 
 public class DButils {
 	
-	private static final String DB_USER = "privategriffe";
-	private static final String DB_PASSWORD = "privategriffe";
+	private static final String DB_USER = "root";
+	private static final String DB_PASSWORD = "Beije03";
 	private static final String DB_URL = "jdbc:mysql://localhost:3306/gestionale?serverTimezone=CET";
 	
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
@@ -21,51 +21,74 @@ public class DButils {
 		return conn;
 	}
 
-	public static void main(String argv[]) {
-		
+	public static void inseriscidipendente (String nome, String cognome,String data_nascita,
+			String luogo_nascita, String genere, String telefono,String email) {
+
 		Connection conn = null;
 		Statement stmt = null;
-		ResultSet rset= null;
 
 		try {
 			conn = DButils.getConnection();
-			
-			String insert = "INSERT INTO tabella_prova VALUES (null, 'edi', 'paperetti', 'M', '20190221')";
-
-			String query = "SELECT * from tabella_prova";
-
-
 			stmt = conn.createStatement();
 
+			String insert = "INSERT INTO dipendente (nome,cognome,data_nascita,luogo_nascita,genere,telefono,Mail) VALUES "
+					+ "('"+nome+"', '"+cognome+"','"+data_nascita+"','"+luogo_nascita+"','"+genere+"','"+telefono+"','"+email+"')";
 			stmt.execute(insert);
-			rset = stmt.executeQuery(query);
-
-			while (rset.next()) {
-				int id = rset.getInt("id");
-				String nome = rset.getString("nome");
-				String cognome = rset.getString("cognome");
-
-				System.out.println("" + id + ", " + nome + ", " + cognome);
-			}
-
-		}
-		catch (SQLException se) {
-			System.out.println("SQLError: " + se.getMessage() + " code: " + se.getErrorCode());
-		}
-		catch(Exception e) {
+		}catch (Exception e){
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
 		finally {
 			try {
-				rset.close();
 				stmt.close();
 				conn.close();
 			}catch (Exception e) {
 				e.printStackTrace();
-			}
+			}	
+		}
+	}
+	
+	public static String visualizzadip(String nome, String cognome) {
+		
+		Connection conn = null;
+		Statement stmt = null;
+		String result = "";
+		String query = "";
+		ResultSet rset = null;
+
+		try {
+			conn = DButils.getConnection();
+			stmt = conn.createStatement();
+			
+			if (nome == "" && cognome == "") query = "select * from dipendente";
+			else if (nome != "" && cognome != "") query = "select * from dipendente where nome = '"+nome+"' && cognome = '"+cognome+"'";
+			else if (nome != "") query = "select * from dipendente where nome = '"+nome+"'";
+			else query =  "select * from dipendente where cognome = '"+cognome+"'";
+			
+		rset = stmt.executeQuery(query);
+		System.out.println(result);
+		if (rset.next()==false) result="Nessun risultato trovato"; 
+		else {
+		while(rset.next()) {
+			
+			result += "ID"+rset.getInt("ID")+", ";
+			result += "nome"+rset.getString("nome")+", ";
+			result += "cognome"+rset.getString("cognome")+"<br/>";
+		}
 		}
 		
+		}catch (Exception e){
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				stmt.close();
+				conn.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}	
+		}
+		return result;
 	}
-
 }
